f8bc00fd5bd50b50b33421d17ec8a25bf7fb8c3b
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-04-15 16:41:32 -0300

    BaSP-W05-2022: ex01-variables and operators

---
+++ Semana-05/Exercises/01_variables_and_operators.js
@@ -0,0 +1,31 @@
+// a
+var firstNumber = 30;
+var secondNumber = 20;
+
+additionNumber = firstNumber + secondNumber;
+
+console.log(firstNumber + ' + ' + secondNumber + ' = ' + additionNumber);
+
+// b
+var firstString = 'Alejandro';
+var secondString = 'Santini';
+
+concatenation = firstString + secondString;
+
+console.log('First string: ' + firstString);
+console.log('Second string: ' + secondString);
+console.log('The concatenation is: ' + concatenation);
+
+// c
+var firstWord = 'Chair';
+var secondWord = 'Box';
+
+firstWordLength = firstWord.length;
+secondWordLenght = secondWord.length;
+numLetters = firstWordLength + secondWordLenght;
+
+console.log('First word: ' + firstWord);
+console.log('Second word: ' + secondWord);
+console.log('The numbers of letters is: ' + numLetters);
+
+
