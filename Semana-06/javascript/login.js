7dc0224fdd09519b02d6fbbd01f8866f7f0fa303
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-04-26 11:27:13 -0300

    BaSP-W06-2022: Login, sign up - js code complete (validation functions are missing)

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -8,16 +8,35 @@ window.onload = function() {
 
     inputEmail.addEventListener('blur', emailValidator);
     inputPassword.addEventListener('blur', passwordValidator);
+    inputEmail.addEventListener('focus', emailFocus);
+    inputPassword.addEventListener('focus', passwordFocus);
     button.addEventListener('click', buttonSubmit);
+
+    function emailFocus(e) {
+        e.preventDefault();
+        inputEmail.classList.remove('email-box-error-i');
+        document.getElementsByClassName('error-message')[0].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[0].style.color = '#373867';
+    }
+
+    function passwordFocus(e) {
+        e.preventDefault();
+        inputPassword.classList.remove('password-box-error-i');
+        document.getElementsByClassName('error-message')[1].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[1].style.color = '#373867';
+    }
 
     function emailValidator(e) {
         e.preventDefault();
         if (inputEmail.value == 'ale') {
             emailValidationRes = inputEmail.value + '   | Valid email |';
-            console.log('1');
+            return true;
         } else {
             emailValidationRes = inputEmail.value + '   | Incomplete or invalid email! |';
-            console.log('2');
+            inputEmail.classList.add('email-box-error-i');
+            document.getElementsByClassName('error-message')[0].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[0].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -25,13 +44,28 @@ window.onload = function() {
         e.preventDefault();
         if (inputPassword.value == '123') {
             passwordValidationRes = inputPassword.value + '  | Valid pasword |';
+            return true;
         } else {
             passwordValidationRes = inputPassword.value + '   | Incomplete or invalid password! |';
+            inputPassword.classList.add('password-box-error-i');
+            document.getElementsByClassName('error-message')[1].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[1].style.color = '#f10000';
+            return false;
         }
     }
 
     function buttonSubmit(e) {
         e.preventDefault();
-        alert('Email: ' + emailValidationRes + '\n' + 'Password: ' + passwordValidationRes);
-    }
+        if (emailValidator(e) == true || passwordValidator(e) == true) {
+            inputEmail.classList.remove('email-box-error-i');
+            document.getElementsByClassName('error-message')[0].style.visibility = 'hidden';
+            document.getElementsByClassName('error-color')[0].style.color = '#373867';
+            inputPassword.classList.remove('password-box-error-i');
+            document.getElementsByClassName('error-message')[1].style.visibility = 'hidden';
+            document.getElementsByClassName('error-color')[1].style.color = '#373867';
+            alert('Email: ' + emailValidationRes + '\n' + 'Password: ' + passwordValidationRes);
+        } else {
+            alert('Data required!');
+        }
+    }      
 }
--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -15,7 +15,7 @@ window.onload = function() {
     var nameValidationRes;
     var surnameValidationRes;
     var dniValidationRes;
-    var birthDatelidationRes;
+    var birthDateValidationRes;
     var phoneValidationRes;
     var addressValidationRes;
     var locationValidationRes;
@@ -35,16 +35,107 @@ window.onload = function() {
     inputEmail.addEventListener('blur', emailValidator);
     inputPassword.addEventListener('blur', passwordValidator);
     inputPassword2.addEventListener('blur', password2Validator);
+    inputName.addEventListener('focus', nameFocus);
+    inputSurname.addEventListener('focus', surnameFocus);
+    inputDni.addEventListener('focus', dniFocus);
+    inputBirthDate.addEventListener('focus', birthDateFocus);
+    inputPhone.addEventListener('focus', phoneFocus);
+    inputAddress.addEventListener('focus', addressFocus);
+    inputLocation.addEventListener('focus', locationFocus);
+    inputPostalCode.addEventListener('focus', postalCodeFocus);
+    inputEmail.addEventListener('focus', emailFocus);
+    inputPassword.addEventListener('focus', passwordFocus);
+    inputPassword2.addEventListener('focus', password2Focus)
     button.addEventListener('click', buttonSubmit);
+
+    function nameFocus(e) {
+        e.preventDefault();
+        inputName.classList.remove('name-box-error-i');
+        document.getElementsByClassName('error-message')[0].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[0].style.color = '#373867';
+    }
+
+    function surnameFocus(e) {
+        e.preventDefault();
+        inputSurname.classList.remove('surname-box-error-i');
+        document.getElementsByClassName('error-message')[1].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[1].style.color = '#373867';
+    }
+
+    function dniFocus(e) {
+        e.preventDefault();
+        inputDni.classList.remove('dni-box-error-i');
+        document.getElementsByClassName('error-message')[2].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[2].style.color = '#373867';
+    }
+
+    function birthDateFocus(e) {
+        e.preventDefault();
+        inputBirthDate.classList.remove('birth-date-box-error-i');
+        document.getElementsByClassName('error-message')[3].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[3].style.color = '#373867';
+    }
+
+    function phoneFocus(e) {
+        e.preventDefault();
+        inputPhone.classList.remove('phone-box-error-i');
+        document.getElementsByClassName('error-message')[4].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[4].style.color = '#373867';
+    }
+
+    function addressFocus(e) {
+        e.preventDefault();
+        inputAddress.classList.remove('address-box-error-i');
+        document.getElementsByClassName('error-message')[5].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[5].style.color = '#373867';
+    }
+
+    function locationFocus(e) {
+        e.preventDefault();
+        inputLocation.classList.remove('location-box-error-i');
+        document.getElementsByClassName('error-message')[6].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[6].style.color = '#373867';
+    }
+
+    function postalCodeFocus(e) {
+        e.preventDefault();
+        inputPostalCode.classList.remove('postal-code-box-error-i');
+        document.getElementsByClassName('error-message')[7].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[7].style.color = '#373867';
+    }
+
+    function emailFocus(e) {
+        e.preventDefault();
+        inputEmail.classList.remove('email-box-error-i');
+        document.getElementsByClassName('error-message')[8].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[8].style.color = '#373867';
+    }
+
+    function passwordFocus(e) {
+        e.preventDefault();
+        inputPassword.classList.remove('password-box-error-i');
+        document.getElementsByClassName('error-message')[9].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[9].style.color = '#373867';
+    }
+
+    function password2Focus(e) {
+        e.preventDefault();
+        inputPassword2.classList.remove('password2-box-error-i');
+        document.getElementsByClassName('error-message')[10].style.visibility = 'hidden';
+        document.getElementsByClassName('error-color')[10].style.color = '#373867';
+    }
 
     function nameValidator(e) {
         e.preventDefault();
         if (inputName.value == 'ale') {
             nameValidationRes = inputName.value + '   | Valid name |';
-            console.log('1');
+            return true;
         } else {
             nameValidationRes = inputName.value + '   | Incomplete or invalid name! |';
-            console.log('2');
+            inputName.classList.add('name-box-error-i');
+            document.getElementsByClassName('error-message')[0].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[0].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -52,10 +143,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputSurname.value == 'ale') {
             surnameValidationRes = inputSurname.value + '   | Valid surname |';
-            console.log('1');
+            return true;
         } else {
             surnameValidationRes = inputSurname.value + '   | Incomplete or invalid surname! |';
-            console.log('2');
+            inputSurname.classList.add('surname-box-error-i');
+            document.getElementsByClassName('error-message')[1].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[1].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -63,10 +157,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputDni.value == 'ale') {
             dniValidationRes = inputDni.value + '   | Valid dni |';
-            console.log('1');
+            return true;
         } else {
             dniValidationRes = inputDni.value + '   | Incomplete or invalid dni! |';
-            console.log('2');
+            inputDni.classList.add('dni-box-error-i');
+            document.getElementsByClassName('error-message')[2].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[2].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -74,10 +171,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputBirthDate.value == 'ale') {
             birthDateValidationRes = inputBirthDate.value + '   | Valid birth date |';
-            console.log('1');
+            return true;
         } else {
             birthDateValidationRes = inputBirthDate.value + '   | Incomplete or invalid birth date! |';
-            console.log('2');
+            inputBirthDate.classList.add('birth-date-box-error-i');
+            document.getElementsByClassName('error-message')[3].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[3].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -85,10 +185,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputPhone.value == 'ale') {
             phoneValidationRes = inputPhone.value + '   | Valid phone |';
-            console.log('1');
+            return true;
         } else {
             phoneValidationRes = inputPhone.value + '   | Incomplete or invalid phone! |';
-            console.log('2');
+            inputPhone.classList.add('birth-date-box-error-i');
+            document.getElementsByClassName('error-message')[4].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[4].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -96,10 +199,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputAddress.value == 'ale') {
             addressValidationRes = inputAddress.value + '   | Valid address |';
-            console.log('1');
+            return true;
         } else {
             addressValidationRes = inputAddress.value + '   | Incomplete or invalid address! |';
-            console.log('2');
+            inputAddress.classList.add('address-box-error-i');
+            document.getElementsByClassName('error-message')[5].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[5].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -107,10 +213,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputLocation.value == 'ale') {
             locationValidationRes = inputLocation.value + '   | Valid location |';
-            console.log('1');
+            return true;
         } else {
             locationValidationRes = inputLocation.value + '   | Incomplete or invalid location! |';
-            console.log('2');
+            inputLocation.classList.add('location-box-error-i');
+            document.getElementsByClassName('error-message')[6].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[6].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -118,10 +227,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputPostalCode.value == 'ale') {
             postalCodeValidationRes = inputPostalCode.value + '   | Valid postal code |';
-            console.log('1');
+            return true;
         } else {
             postalCodeValidationRes = inputPostalCode.value + '   | Incomplete or invalid postal code! |';
-            console.log('2');
+            inputPostalCode.classList.add('postal-code-box-error-i');
+            document.getElementsByClassName('error-message')[7].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[7].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -129,10 +241,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputEmail.value == 'ale') {
             emailValidationRes = inputEmail.value + '   | Valid email |';
-            console.log('1');
+            return true;
         } else {
             emailValidationRes = inputEmail.value + '   | Incomplete or invalid email! |';
-            console.log('2');
+            inputEmail.classList.add('email-box-error-i');
+            document.getElementsByClassName('error-message')[8].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[8].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -140,8 +255,13 @@ window.onload = function() {
         e.preventDefault();
         if (inputPassword2.value == '123') {
             passwordValidationRes = inputPassword.value + '  | Valid pasword |';
+            return true;
         } else {
             passwordValidationRes = inputPassword.value + '   | Incomplete or invalid password! |';
+            inputPassword.classList.add('password-box-error-i');
+            document.getElementsByClassName('error-message')[9].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[9].style.color = '#f10000';
+            return false;
         }
     }
 
@@ -149,14 +269,19 @@ window.onload = function() {
         e.preventDefault();
         if (inputPassword2.value == '123') {
             password2ValidationRes = inputPassword2.value + '  | Valid pasword |';
+            return true;
         } else {
             password2ValidationRes = inputPassword2.value + '   | Incomplete or invalid password! |';
+            inputPassword2.classList.add('password2-box-error-i');
+            document.getElementsByClassName('error-message')[10].style.visibility = 'visible';
+            document.getElementsByClassName('error-color')[10].style.color = '#f10000';
+            return false;
         }
     }
 
     function buttonSubmit(e) {
         e.preventDefault();
-        alert(+ 'Name: ' + nameValidationRes + '\n'
+        alert('Name: ' + nameValidationRes + '\n'
             + 'Surname: ' + surnameValidationRes + '\n'
             + 'DNI: ' + dniValidationRes + '\n'
             + 'Birth daste: ' + birthDateValidationRes + '\n'
